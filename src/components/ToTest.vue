2fa36ba5d9e67d9424805775a0dc9179214e6f5e
John Steban Lopez <104475729+JohnGolgota@users.noreply.github.com>
2023-10-02 14:50:13 -0500

    feat(actions and testing)

---
+++ src/components/ToTest.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		<h1>My Component</h1>
+		<input type="text" v-model="inputValue">
+		<button @click="handleClick">Submit</button>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			inputValue: ''
+		}
+	},
+	methods: {
+		handleClick() {
+			console.log(this.inputValue)
+			// do something with the input value
+		}
+	}
+}
+</script>
---
+++ src/components/__test__/ToTest.test.js
@@ -0,0 +1,6 @@
+/**
+ * @vitests-environment happy-dom
+ */
+import ToTest from '../ToTest';
+import { describe, it, expect } from 'vitest';
+import { mount } from '@vue/test-utils';
