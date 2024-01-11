c73cce5bc96f57c8d3f76691e1197f69fa03129a
John Steban Lopez <104475729+JohnGolgota@users.noreply.github.com>
2024-01-11 10:48:00 -0500

    feat(iter 1) color selects

--- README.md
+++ README.md
@@ -5,3 +5,12 @@ This template should help get you started developing with Vue 3 in Vite. The tem
 ## Recommended IDE Setup
 
 - [VS Code](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin).
+
+## Compilacion para produccion
+
+```bash
+# Compilar para produccion
+npm run build:portada:prod
+```
+
+Esta pendiente la --base de la ruta de produccion
--- src/components/select-s.vue
+++ src/components/select-s.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="form-floating">
-		<select class="form-select" :id="inputId"  @change="$emit('update:selectValue', $event.target.value)" :value="selectedValue" :aria-label="label" :disabled="!editable" required>
+		<select class="form-select" :id="inputId" @change="$emit('update:selectValue', $event.target.value)" :value="selectedValue" :aria-label="label" :disabled="!editable" required>
 			<option :value="''"></option>
 			<option v-for="option in options" :key="option.value" :value="option.value">{{ option.label }}</option>
 		</select>
@@ -18,5 +18,15 @@ const props = defineProps({
 	editable: Boolean,
 })
 
-defineEmits (['update:selectValue'])
+defineEmits(['update:selectValue'])
 </script>
+
+<style scoped>
+:root {
+	--text-select-color: #000;
+}
+
+.form-select>option {
+	color: var(--text-select-color);
+}
+</style>
